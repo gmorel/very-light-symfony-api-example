defbda91a1a4302dce28bdf78b00b70e6750442d
Guillaume MOREL <me@gmorel.io>
2020-07-01 19:00:37 +0200

    Add s suffix to REST resource url

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("v1/product")
+ * @Route("v1/products")
  */
 class ProductController extends AbstractController
 {
